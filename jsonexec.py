22f1f8a514affbc0ea09cc80992f22fac265e09a
Broote <dmitriy.gershun@gmail.com>
2012-10-19 16:42:27 +0400

    same

--- jsonexec.py
+++ jsonexec.py
@@ -17,10 +17,14 @@ def fmore(i):
 	
 def fless(i):
 	return int(i['left']) < int(i['right'])
+	
+def fif(i):
+	# if expr(i['first')
+	return 0
 
 
 
-text = json.loads('{"program":[{"operation":"-", "left":"5", "right":"7"}, {"operation":"==", "left":"6", "right":"7"}]}')
+text = json.loads('{"program":[{"operation":"if", "first":"5<7", "second":"2-3", "third":"4-2"}, {"operation":"==", "left":"6", "right":"7"}]}')
 prog = text['program']
 for i in prog:
 	if i['operation'] == "+":
@@ -37,5 +41,7 @@ for i in prog:
 		print(fless(i))
 	elif i['operation'] == "==":
 		print(fmore(i) or fless(i) == False)
+	elif i['operation'] == "if":
+		print(fif(i))
 
 
