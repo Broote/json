9fa04e7ca3307d3008c68a370a18e7f87f828e5c
Dmitry Gershun <dmitriy.gershun@gmail.com>
2013-02-10 05:29:15 +0400

    added linear call and input to json

--- jsonexec.py
+++ jsonexec.py
@@ -3,27 +3,30 @@ import pdb
 #pdb.set_trace()
 #implemented +, -, *, /, &&, ||, >, <, ==, if
 
+
 def get_by_path(data, array):
-  current_path = data
-  for i in range(len(array)):
-    current_path = current_path[array[i]]
-  return current_path
+    current_path = data
+    for i in range(len(array)):
+        current_path = current_path[array[i]]
+    return current_path
+
 
 def binary_operation(current, text):
-  left = current['left']
-  right = current['right']
-  if left['type'] == "pointer":
-    left = get_by_path(text, left['path'])
-    right = get_by_path(text, right['path'])
-    return left, right
-    
+    left = current['left']
+    right = current['right']
+    if left['type'] == "pointer":
+        left = get_by_path(text, left['path'])
+        right = get_by_path(text, right['path'])
+        return left, right
+
+
 def get_next(next):
-  if next == "end":
-    return "end"
-  elif next['type'] == "pointer":
-    # возвращается text['program'][number]
-    # подумать, стоит ли каждый раз передавать program
-    return text[next['path'][0]][next['path'][1]]
+    if next == "end":
+        return "end"
+    elif next['type'] == "pointer":
+        # возвращается text['program'][number]
+        # подумать, стоит ли каждый раз передавать program
+        return text[next['path'][0]][next['path'][1]]
 
 f = open('json_prog', 'r')
 text = json.loads(str(f.read()))
@@ -36,51 +39,51 @@ data = text['data']
 # входная точка
 next = prog[1]
 while (next != "end"):
-  current = next
-  if current['operation'] == "linear_plus":
-    left, right = binary_operation(current, text)
-    current['result'] = left + right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_minus":
-    left, right = binary_operation(current, text)
-    current['result'] = left - right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_mult":
-    left, right = binary_operation(current, text)
-    current['result'] = left * right
-    next = get_next(current['next'])
+    current = next
+    if current['operation'] == "linear_plus":
+        left, right = binary_operation(current, text)
+        current['result'] = left + right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_minus":
+        left, right = binary_operation(current, text)
+        current['result'] = left - right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_mult":
+        left, right = binary_operation(current, text)
+        current['result'] = left * right
+        next = get_next(current['next'])
   # не забыть про дробные
-  elif current['operation'] == "linear_div":
-    left, right = binary_operation(current, text)
-    current['result'] = left / right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_and":
-    left, right = binary_operation(current, text)
-    current['result'] = left and right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_or":
-    left, right = binary_operation(current, text)
-    current['result'] = left or right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_more":
-    left, right = binary_operation(current, text)
-    current['result'] = left > right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_less":
-    left, right = binary_operation(current, text)
-    current['result'] = left < right
-    next = get_next(current['next'])
-  elif current['operation'] == "linear_equal":
-    left, right = binary_operation(current, text)
-    current['result'] = left == right
-    next = get_next(current['next'])    
-  elif current['operation'] == "linear_if":
-    if current['condition']['type'] == "pointer":
-      condition = get_by_path(text, current['condition']['path'])
-      if condition == True:
-        next = get_next(current['if_true']['next'])
-      if condition == False:
-        next = get_next(current['if_false']['next'])  
+    elif current['operation'] == "linear_div":
+        left, right = binary_operation(current, text)
+        current['result'] = left / right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_and":
+        left, right = binary_operation(current, text)
+        current['result'] = left and right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_or":
+        left, right = binary_operation(current, text)
+        current['result'] = left or right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_more":
+        left, right = binary_operation(current, text)
+        current['result'] = left > right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_less":
+        left, right = binary_operation(current, text)
+        current['result'] = left < right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_equal":
+        left, right = binary_operation(current, text)
+        current['result'] = left == right
+        next = get_next(current['next'])
+    elif current['operation'] == "linear_if":
+        if current['condition']['type'] == "pointer":
+            condition = get_by_path(text, current['condition']['path'])
+            if condition == True:
+                next = get_next(current['if_true']['next'])
+            if condition == False:
+                next = get_next(current['if_false']['next'])
 
 print("finish")
 print(text)
